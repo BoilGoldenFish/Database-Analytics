80face99dd62ae13d98ff43be0329e6d3e9dd6b7
Lainey <jil172@ucsd.edu>
2022-04-26 23:43:28 +0000

    hi

---
+++ koimango.com/public_html/cgi-bin/py-sessions-1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Cache-Control: no-cache")
+print("<html>")
+print("<head>")
+print("<title>Perl Sessions</title>")
+print("</head>")
+print("<body>")
+
+print("<h1>Perl Sessions Page 1</h1>")
+
+print("</body></html>")
