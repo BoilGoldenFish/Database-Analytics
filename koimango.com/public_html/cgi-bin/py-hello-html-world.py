1a2ed810c5a9bca046cf99d2c1e290c7290da1e6
Lainey <jil172@ucsd.edu>
2022-04-26 00:52:21 +0000

    config for python

---
+++ koimango.com/public_html/cgi-bin/py-hello-html-world.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from datetime import datetime
+import cgitb
+import socket
+
+cgitb.enable()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+now = datetime.now()
+
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html\n\n")
+print("<html><head><title>Hello CGI World</title></head>\
+    <body><h1 align=center>Hello HTML World</h1>\
+  	<hr/>\n")
+
+print("Hello World<br/>\n")
+print("This program was generated at: {}\n<br/>".format(now.strftime("%A %B %d %H:%M:%S %Y")))
+print("Your current IP address is: {}<br/>".format(s.getsockname()[0]))
+
+print("</body></html>")
