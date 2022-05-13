77896ce99999bde9768e739b1851170f4d57257b
Lainey <jil172@ucsd.edu>
2022-05-13 23:08:34 +0000

    --noediy

--- koimango.com/public_html/collector.js
+++ koimango.com/public_html/collector.js
@@ -1,5 +1,5 @@
 localStorage.setItem("user agent", window.navigator.userAgent);
 localStorage.setItem("language", window.navigator.language);
 localStorage.setItem("allow cookies", navigator. cookieEnabled);
-localStorage.setItem("allow JavaScript", True);
+
 
