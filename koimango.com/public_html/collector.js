e175749f7ec73ec154d4bcdd50a5ad5631888275
Lainey <jil172@ucsd.edu>
2022-05-14 23:13:58 +0000

    --noedit

--- koimango.com/public_html/collector.js
+++ koimango.com/public_html/collector.js
@@ -1,5 +1,15 @@
-localStorage.setItem("user agent", window.navigator.userAgent);
-localStorage.setItem("language", window.navigator.language);
-localStorage.setItem("allow cookies", navigator. cookieEnabled);
+window.onload = (event) => {
+    localStorage.setItem("user agent", window.navigator.userAgent);
+    localStorage.setItem("language", window.navigator.language);
+    localStorage.setItem("allow cookies", navigator. cookieEnabled);
+    localStorage.setItem("allow javascript", true);
+    
+    let element = document.querySelector("#checkCSS");
+    if (window.getComputedStyle(element).getPropertyValue('text-transform') != 'uppercase'){
+        localStorage.setItem('allow CSS', false);
+    } else {
+        localStorage.setItem('allow CSS', true);
+    }
 
-
+    
+};
